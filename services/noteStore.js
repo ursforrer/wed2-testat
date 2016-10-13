fba5d555f83d548b34fe83fa10d49b254beee38d
Urs Forrer <mail@ursforrer.ch>
2016-10-13 11:25:00 +0200

    Add db-connection to service/nodeStore.js
    Add db-connection to service/nodeStore.js

--- services/noteStore.js
+++ services/noteStore.js
@@ -10,4 +10,34 @@ function Note(noteTitle, noteDescription, noteDueDate, noteImportance) {
     this.duedate = noteDueDate;
     this.importane = noteImportance;
     this.finished = false;
+    this.state = "OK";
 }
+
+function publicAddNote(noteTitle, noteDescription, noteDueDate, noteImportance, callback ) {
+    var note = new Note(noteTitle, noteDescription, noteDueDate, noteImportance);
+    db.insert(note , function (err, newDoc) {
+        if (callback) {
+            callback(err, newDoc);
+        }
+    });
+}
+
+function publicRemove(id, callback) {
+    db.update({_id: id}, {$set: {"state": "DELETED"}}, {}, function (err, doc) {
+        publicGet(id, callback);
+    });
+}
+
+function publicGet(id, callback) {
+    db.findOne({ _id: id}, function (err, doc) {
+        callback(err, doc);
+    });
+}
+
+function publicAll() {
+    db.find({}, function (err, docs) {
+        callback(err,docs);
+    });
+}
+
+module.exports = {add : publicAddNote, delete : publicRemove, get : publicGet, all : publicAll};
