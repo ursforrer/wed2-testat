83cc99a24f8074796f485dc32a812eff4dccfde2
Urs Forrer <mail@ursforrer.ch>
2016-11-13 14:34:26 +0100

    Implemented sort finally
    Implemented sort finally

--- controller/notesController.js
+++ controller/notesController.js
@@ -7,24 +7,40 @@ module.exports.showIndex = function(req, res)
 {
     var orderBy = req.query['orderBy'];
     var orderParameter;
+    var orderParameterTwo;
+    var sort;
     if (orderBy === undefined || orderBy === null) {
         // Falls nichts mitgegeben wurde, sollten die Werte so belassen werden.
         if (req.cookies.sortby === undefined || req.cookies.sortby === null) {
             // Initial, falls keine Cookies vorhanden sind.
             orderParameter = "duedate";
             res.cookie("sortby", orderParameter);
+            res.cookie("sort", "1");
         }
         else {
             // Ansonsten vorherigen Cookieparameter auslesen
             orderParameter = req.cookies.sortby;
+            orderParameterTwo = req.cookies.sort;
         }
     }
     else {
         // Werte setzen, wenn etwas über die Queryparameter mitgegeben wurde.
         res.cookie("sortby", orderBy);
+        if (orderBy == req.cookies.sortby) {
+            if (req.cookies.sort == "-1") {
+                orderParameterTwo = "1";
+            }
+            else {
+                orderParameterTwo = "-1";
+            }
+        }
+        else {
+            orderParameterTwo = "1";
+        }
+        res.cookie("sort", orderParameterTwo);
         orderParameter = orderBy;
     }
-    store.all(orderParameter, "1", "", function (err, notes) {
+    store.all(orderParameter, orderParameterTwo, "", function (err, notes) {
         res.render("index", {'notes' : notes});
     })
 };
